ab8a00ff4d45f90d6d0dab96975ce0aa8e474740
Diana <diana-maria.chelea@student.tuiasi.ro>
2025-05-29 23:14:10 +0300

    yolov8x

--- train_yolo8.py
+++ train_yolo8.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8x.pt")
 
 model.train(
     data="dataset/data.yaml", 
-    epochs=150,
+    epochs=200,
     imgsz=640,
     batch=32,
     device='cuda'  
